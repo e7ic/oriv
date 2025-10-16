692d6a124128cbe15189a9f8632b1d029a3fdb18
e7ic <i@coooooc.com>
2025-10-16 21:57:37 +0800

    chore: 在.gitignore中添加.claude目录
    避免将.claude目录中的文件提交到版本控制

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::error::Error;
+use i_slint_backend_winit::WinitWindowAccessor;
 
 slint::include_modules!();
 
@@ -13,6 +14,48 @@ fn main() -> Result<(), Box<dyn Error>> {
         move || {
             let ui = ui_handle.unwrap();
             ui.set_counter(ui.get_counter() + 1);
+        }
+    });
+
+    // 最小化窗口
+    ui.on_minimize_window({
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui = ui_handle.unwrap();
+            ui.window().set_minimized(true);
+        }
+    });
+
+    // 最大化/恢复窗口
+    ui.on_maximize_window({
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui = ui_handle.unwrap();
+            let window = ui.window();
+            let is_maximized = window.is_maximized();
+            window.set_maximized(!is_maximized);
+            !is_maximized  // 返回新的最大化状态
+        }
+    });
+
+    // 关闭窗口
+    ui.on_close_window({
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui = ui_handle.unwrap();
+            ui.window().hide().ok();
+            std::process::exit(0);
+        }
+    });
+
+    // 开始拖动窗口
+    ui.on_start_drag_window({
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui = ui_handle.unwrap();
+            ui.window().with_winit_window(|winit_window| {
+                let _ = winit_window.drag_window();
+            });
         }
     });
 
